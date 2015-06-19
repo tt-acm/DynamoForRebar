139d5dc3f4588bced2b9707761333f1052502e39
moethu <maximilian.o.e.thumfart@gmail.com>
2015-06-19 14:10:48 +0100

    Test Project Added

---
+++ Dynamo.Rebar.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Autodesk.Revit.DB;
+
+namespace Dynamo.Rebar.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Curve curve1 = (Curve)Line.CreateBound(new XYZ(0,0,0), new XYZ(10,0,0));
+            Curve curve2 = (Curve)Line.CreateBound(new XYZ(0,10,0), new XYZ(10,10,0));
+
+            XYZ[] points = curve1.Divide(4);
+
+            curve1.MorphTo(curve2,4);
+
+            Assert.IsNull(points);
+        }
+    }
+}
