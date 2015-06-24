35eba1b79d08a0dcaca80c1600cababb345b0b71
moethu <maximilian.o.e.thumfart@gmail.com>
2015-06-24 14:56:21 +0100

    Headers Added

--- src/Dynamo.Rebar/Properties/AssemblyInfo.cs
+++ src/Dynamo.Rebar/Properties/AssemblyInfo.cs
@@ -1,16 +1,33 @@
-﻿using System.Reflection;
+﻿//
+// Copyright 2015 Autodesk, Inc.
+// Author: Thornton Tomasetti Ltd, CORE Studio
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RevitRebar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Revit.Rebar")]
+[assembly: AssemblyDescription("Revit Dynamo Rebar Tools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Thornton Tomasetti, Inc.")]
-[assembly: AssemblyProduct("RebarMassing")]
-[assembly: AssemblyCopyright("Copyright © Thornton Tomasetti, Inc. 2015")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
+[assembly: AssemblyProduct("Revit.Rebar")]
+[assembly: AssemblyCopyright("Copyright © Autodesk, Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +51,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
