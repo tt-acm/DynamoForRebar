ecaef429b1382abe92b3e1f36917cfdeaf76ece6
moethu <maximilian.o.e.thumfart@gmail.com>
2015-09-02 11:17:36 +0100

    Testing Framework

--- src/Dynamo.Rebar_Debug.bat
+++ src/Dynamo.Rebar_Debug.bat
@@ -1,4 +1,4 @@
 set DYNAMO_API=C:\Program Files\Dynamo 0.8
-set REVIT_API=C:\Program Files\Autodesk\Revit 2015
-set REVIT_VERSION=Revit_2015
+set REVIT_API=C:\Program Files\Autodesk\Revit 2016
+set REVIT_VERSION=Revit_2016
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "Dynamo.Rebar.sln"
---
+++ src/DynamoRebarTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+
+namespace DynamoRebarTest
+{
+    //[SetUp]
+    //public void Setup()
+    //{
+        // Set the working directory. This will allow you to use the OpenAndRunDynamoDefinition method,
+        // specifying a relative path to the .dyn file you want to test.
+
+     //   var asmDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    //    workingDirectory = Path.GetFullPath(Path.Combine(asmDirectory, @"..\..\..\exampleFiles"));
+   // }  
+
+    [TestFixture]
+    public class SystemTestExample : RevitSystemTestBase
+    {
+        [Test, TestModel(@"..\..\..\..\TextFiles\RevitProject\RevitProject.rvt")]
+        public void Location()
+        {
+            OpenAndRunDynamoDefinition(@"..\..\..\..\TextFiles\DynamoDefinitions\Tag.dyn");
+
+            // Your test logic goes here.
+        }
+    }
+}
